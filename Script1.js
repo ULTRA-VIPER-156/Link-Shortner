3424e23fc89a268b2350dadcf4bcaf5b05348e34
Jonathan <netsijonathan@gmail.com>
2025-12-28 00:30:59 +0200

    Enhance URL shortening with loader and copy features
    Added loader and copy functionality to URL shortening process.

--- Script1.js
+++ Script1.js
@@ -5,10 +5,10 @@ const UrlInput = document.getElementById("url-input");
 const shortendUrl = document.getElementById("shortened-url");
 const copyBtn = document.getElementById("copy-btn");
 const submitBtn = document.getElementById("submit-btn");
-const CopyMesaage = document.getElementById("copy-message");
 const errorMsg = document.getElementById("error-message");
-const result = document.getElementById("result");
-
+const result = document.getElementById("short-url");
+const loader = document.getElementById("loader");
+const copyStuff = document.getElementById("copy-Stuff");
 //so basically the idea here is to make a request to tiny url using their api 
 //so what happens is we sen over our long url and tinyurl sends back a shortend url
 
@@ -17,7 +17,7 @@ async function shortenUrl(longUrl)
   const apiUrl  ="https://api.tinyurl.com/create";
   //I know its not a good idea to hard code the token here 
   //but fuck it  its just a demo
-  const token = "uztxeuz04MxBH264IobUhwaOew2iHsqhy5upnLQRpXoepi8MuUSWEIqRLBpz";
+  const token = "OhTG3rQRyLZ7Q721gkPON5cE7rP2FIVK9dnOPV6EdppIbrFa5G90fDfOWNMM";
 
   try {
     const response = await fetch(apiUrl,{
@@ -60,6 +60,7 @@ form.addEventListener("submit",async (e)=>{
 
   submitBtn.innerText="Shortening...";
   submitBtn.disabled=true;
+  loader.style.display="block";
   errorMsg.style.display="none";
   result.style.display="none";
   copyBtn.style.display="none";
@@ -72,6 +73,8 @@ form.addEventListener("submit",async (e)=>{
     shortendUrl.innerText=shrtUrl;
     result.style.display="block";
     copyBtn.style.display="block";
+    copyStuff.style.display="block";
+    loader.style.display="none";
   }
 
   submitBtn.disabled=false;
